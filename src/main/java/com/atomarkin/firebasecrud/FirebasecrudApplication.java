76ba789d75e6f4c11d7c1f027106ff74d493f8b2
Jonathan Ato Markin <jona10711@gmail.com>
2023-03-19 12:39:41 +0100

    Initial

---
+++ src/main/java/com/atomarkin/firebasecrud/FirebasecrudApplication.java
@@ -0,0 +1,13 @@
+package com.atomarkin.firebasecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebasecrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebasecrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atomarkin/firebasecrud/FirebasecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atomarkin.firebasecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebasecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
